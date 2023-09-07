abbbb37e08740866cfff24137da46aa81978b527
RedRayz <71994877+RedRayz@users.noreply.github.com>
2023-09-08 00:40:38 +0900

    Fix ArgumentOutOfRangeException when dragging a row to an empty space on Image Tags

--- BooruDatasetTagManager/AutoCompleteTextBox.cs
+++ BooruDatasetTagManager/AutoCompleteTextBox.cs
@@ -6,7 +6,6 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Diagnostics;
 
 namespace BooruDatasetTagManager
 {
