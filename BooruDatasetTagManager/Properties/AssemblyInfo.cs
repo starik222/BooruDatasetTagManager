066bc4d00b685cdc1fab384c9c3a3824038f9b05
starik222 <starik222@mail.ru>
2024-02-22 14:34:08 +0700

    Change version

--- BooruDatasetTagManager/Properties/AssemblyInfo.cs
+++ BooruDatasetTagManager/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.1.1.0")]
+[assembly: AssemblyFileVersion("2.1.1.0")]
