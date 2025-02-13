e5437bdbc120bcb4d98d5ce4f1b0ed1501f0ce0d
starik222 <starik222@mail.ru>
2025-02-13 10:35:53 +0700

    Change version

--- BooruDatasetTagManager/Properties/AssemblyInfo.cs
+++ BooruDatasetTagManager/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BooruDatasetTagManager")]
-[assembly: AssemblyCopyright("Copyright ©  2022-2023")]
+[assembly: AssemblyCopyright("Copyright ©  2022-2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.4.0")]
-[assembly: AssemblyFileVersion("2.4.0.0")]
+[assembly: AssemblyVersion("2.4.1")]
+[assembly: AssemblyFileVersion("2.4.1.0")]
